8ba054fbd20d01f375ccff8fbb982e7c81935aa6
f74032170 <f74032170@ncku.edu.tw>
2016-06-19 12:06:55 +0800

    Create Object.cs

---
+++ Object.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tank
+{
+    class Object : PictureBox
+    {
+        private bool desstoryable;
+        private bool driveable;
+        private bool crossable;
+        /*
+        public Object()
+        {
+            InitializeComponent();
+        }
+
+        public Object(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+         */
+       
+
+    }
+}
